2fb38659cf7292d573535123fc59c097f3a0c3e5
makermatic <makermatic@users.noreply.github.com>
2020-05-03 11:26:52 -0400

    reworked methods

--- Icon.js
+++ Icon.js
@@ -4,35 +4,34 @@ class Icon {
     this.playerName = playerName
     this.image
     this.x = 150
-    this.x
     this.y = 0
-    this.up = 0
-    this.down = 0
+    this.width = 0
+    this.height = 0
     this.speed = 35
-    this.p1
-    this.p2
   }
 
-  display() {
+  loadImages() {
     this.p1 = loadImage("assets/P1.png")
     this.p2 = loadImage("assets/P2.png")
+  }
+
+  imagePick() {
     if (this.player === "player1") {
       this.image === this.p1
+      this.x === 150
     }
     else if (this.player === "player2") {
       this.image === this.p2
-      this.x = 100
+      this.x = 400
     }
     else {
       console.log("Master Zack Says: Failed to load images, you may want to check the spelling of your images again.")
     }
-    
-    fill("white")
-    ellipse(this.x, this.y, this.up, this.down)
-    text(this.playerName, this.x, this.y - 50)
   }
 
-  move() {
-
+  display() {
+    fill("blue")
+    ellipse(this.x, this.y, this.width, this.height)
+    text(this.playerName, this.x, this.y - 50)
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,29 +1,26 @@
 //global vars
 let subFont
-let timer
-let lightShade
-let darkShade
-
-let counter = 0
-
+let p1
+let p2
 
 function preload() {
+  p1 = new Icon("player1", "Kiddo")
+  p1.loadImages()
+  p2 = new Icon("player2", "Skull Kid")
+  p2.loadImages()
   subFont = loadFont("/assets/proxima.otf")
-  lightShade = loadImage("/assets/Light Shade.png")
-  darkShade = loadImage("/assets/Dark Shade.png")
 }
 
 function setup() {
   createCanvas(1920, 1080)
-  frameRate(30) //<--cap framerate to match gif
+  frameRate(30) //cap framerate to match gif
+  background ("beige") //added for testing purposes
+  textFont(subFont)
 }
 
 function draw() {
-
-  p1 = new Icon("player1", "Kiddo")
+  p1.imagePick()
   p1.display()
-
-  p2 = new Icon("player2", "Skull Kid")
+  p2.imagePick()
   p2.display()
-
 }
