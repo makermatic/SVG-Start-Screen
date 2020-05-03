af5dd51a3467b8a5626698dccf68b6bacd494d27
makermatic <makermatic@users.noreply.github.com>
2020-05-03 11:04:26 -0400

    Removed background and shades

--- sketch.js
+++ sketch.js
@@ -1,6 +1,5 @@
 //global vars
 let subFont
-let rl
 let timer
 let lightShade
 let darkShade
@@ -10,7 +9,6 @@ let counter = 0
 
 function preload() {
   subFont = loadFont("/assets/proxima.otf")
-  rl = loadImage("/assets/RL_ Background.jpg")
   lightShade = loadImage("/assets/Light Shade.png")
   darkShade = loadImage("/assets/Dark Shade.png")
 }
@@ -18,34 +16,14 @@ function preload() {
 function setup() {
   createCanvas(1920, 1080)
   frameRate(30) //<--cap framerate to match gif
-  background(rl)
 }
 
 function draw() {
 
-  p1 = new Icon("player1")
+  p1 = new Icon("player1", "Kiddo")
   p1.display()
 
   p2 = new Icon("player2", "Skull Kid")
   p2.display()
 
-}
-
-function Shades() {
-  var leftShade = {
-    x: 100, 
-    y: 100, 
-    scaleX: 1920,
-    scaleY: 1080,
-    counter: 0,
-    image (lightShade, x, y, scaleX, scaleY)
-
-  var RightShade = {
-    x: 100, 
-    y: 100, 
-    x2: 100, 
-    y2: 100,
-    image ()
-  }
-  }
 }
