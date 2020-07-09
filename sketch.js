4d58bd1b2f426b7d34642a4e5a7efaa2c9d6d9a2
makermatic <44180512+makermatic@users.noreply.github.com>
2020-07-09 13:43:51 -0400

    Updt names/icons for 7.9

--- sketch.js
+++ sketch.js
@@ -7,8 +7,8 @@ let p2
 // let timer = 300
 
 function preload() {
-  p1 = new Icon("player1", "Campers")
-  p2 = new Icon("player2", "Counselors")
+  p1 = new Icon("player1", "Emagination A")
+  p2 = new Icon("player2", "Emagination B")
   subFont = loadFont("/assets/proxima.otf")
   vs = loadFont("/assets/passion.otf")
 }
